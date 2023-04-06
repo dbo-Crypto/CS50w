d4ef9d2ee1964a52feacf611b8c57c4275ec02f3
dbo-Crypto <damienbourgery@gmail.com>
2023-04-06 15:14:57 +0200

    button

--- mail/mail/static/mail/inbox.js
+++ mail/mail/static/mail/inbox.js
@@ -37,12 +37,18 @@ function load_mailbox(mailbox) {
   .then(response => response.json())
   .then(emails => {
     // Print emails
+    const ElemsContDiv = document.createElement('div');
+    document.querySelector('#emails-view').append(ElemsContDiv);
+    console.log("cont created")
+    const DivBody = document.createElement('div');
+    
+    console.log("Div Body created")
     emails.forEach(element => {
       
-      const newDiv = document.createElement('div');
-      newDiv.setAttribute('id','mail'+element.id);
-      newDiv.setAttribute('class','mail_item');
-      newDiv.innerHTML =
+      const ElemDiv = document.createElement('div');
+      ElemDiv.setAttribute('id','mail'+element.id);
+      ElemDiv.setAttribute('class','mail_item');
+      ElemDiv.innerHTML =
       '<div class="row">' +   
         '<div class="col">' + 
           '<p class="sender">' + element.sender + '&emsp;' + '</p>' +
@@ -50,33 +56,54 @@ function load_mailbox(mailbox) {
         '<div class="col">' + 
           '<p class="subject">' + element.subject + '&emsp;' + '</p>' +
         '</div>' + 
-        '<div class="col-6">' + 
-          '<p class="body">' + element.timestamp + '</p>'
+        '<div class="col">' + 
+          '<p class="body">' + element.timestamp + '</p>'+
+        '</div>' + 
+        '<div class="col-2">' + 
+          '<button id="archive onClick="">Archive</button>'+
         '</div>' + 
       '</div>';
-
-      newDiv.addEventListener('click', function() {
+      
+      ElemDiv.addEventListener('click', function() {
         fetch('/emails/'+element.id)
           .then(response => response.json())
           .then(email => {
               // Print email
-              const DivBody = document.createElement('div');
+              
               DivBody.setAttribute('id','body'+email.id);
               DivBody.setAttribute('class','mail_body');
               DivBody.innerHTML = 
-              '<p class="sender">' + email.body + '</p>';
-              document.querySelector('#emails-view').append(newDivBody);
+                '<p class="category">' + "Sender: " +email.sender + '<br>' + '</p>' +
+                '<p class="category">' + "Recipients: " +email.recipients + '<br>' + '</p>' +
+                '<p class="category">' + "Subject: " +email.subject + '<br>' + '</p>' +
+                '<p class="category">' + "Timestamp: " +email.timestamp + '<br>' + '</p>' +
+                '<p class="category">' +email.body + '<br>' + '</p>' ;
             });
+            fetch('/emails/'+element.id, {
+              method: 'PUT',
+              body: JSON.stringify({
+                  read : true
+              })
+              
+            })
         });
-      document.querySelector('#emails-view').append(newDiv);
+      //const button = document.getElementById("archive");
+      //button.addEventListener('click', function(){
+        //window.alert('button clicked');
+      //});
+      
+      document.querySelector('#emails-view').append(ElemsContDiv);
+      document.querySelector('#emails-view').append(DivBody);
+      ElemsContDiv.appendChild(ElemDiv);
+
       console.log(element)
       console.log('mail'+element.id)
       console.log(element.read)
       if (element.read == false){
-        document.getElementById('mail'+element.id).style.backgroundColor = "red";
+        document.getElementById('mail'+element.id).style.backgroundColor = "white";
         console.log("already false")
       } else {
-        document.getElementById('mail'+element.id).style.backgroundColor = "blue";
+        document.getElementById('mail'+element.id).style.backgroundColor = "gray";
         console.log("set to false")
       }
     });
